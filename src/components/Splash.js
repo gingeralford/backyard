6ad6542042cd584a472c24608d12a4b9cf6b84f9
Ginger Alford <gingeralford@gmail.com>
2021-07-08 02:27:01 -0400

    fixed splash background image issues

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import heroImg from '../assets/backyard - colorful produce.jpg'
+
 
 const Splash = () => {
     return ( 
     <div>
-    <div style={{height: "100vh", position: "fixed", top:0, zIndex: 0}}>
+    <div className="heroImg" >
         {/* <img src={heroImg} alt="colorful and exotic produce in a farmstand" height="100%" className="heroImg"/> */}
-        
     </div>
     
     <div className="splashText">
-    <div className="navstyle" style={{padding: "10px 0"}}>The Future of food is local, again.</div>
+
+    <div className="navstyle" style={{padding: "10px 0"}}>The future of food is local, again.</div>
     Halloumi edam cheese and biscuits. Smelly cheese halloumi bavarian bergkase cheese and wine queso bavarian bergkase cheesecake dolcelatte. Parmesan queso cheese strings blue castello halloumi swiss cheeseburger airedale. Cheese strings lancashire cheese slices hard cheese parmesan the big cheese mozzarella monterey jack. Manchego melted cheese croque monsieur babybel the big cheese say cheese gouda pepper jack. Cheesy feet croque monsieur manchego halloumi cheesy feet cow rubber cheese airedale. Jarlsberg red leicester taleggio melted cheese stinking bishop roquefort mascarpone babybel. Croque monsieur red leicester stinking bishop queso bavarian bergkase red leicester st. agur blue cheese croque monsieur. Halloumi cheesy feet feta pepper jack bavarian bergkase fondue ricotta cheese slices. When the cheese comes out everybody's happy cut the cheese when the cheese comes out everybody's happy when the cheese comes out everybody's happy mascarpone say cheese cheeseburger parmesan. Paneer roquefort queso mozzarella bocconcini red leicester.
-
-Cheese on toast cheddar chalk and cheese. Say cheese gouda emmental fromage hard cheese swiss rubber cheese cheesecake. Red leicester cream cheese gouda pecorino caerphilly croque monsieur parmesan caerphilly. Cauliflower cheese who moved my cheese fromage cut the cheese dolcelatte croque monsieur cow roquefort. Cheesecake blue castello fromage cottage cheese airedale cheese on toast cheesy grin airedale. Everyone loves brie airedale fromage cheesecake swiss bavarian bergkase stilton. Caerphilly danish fontina edam croque monsieur the big cheese cheesecake fromage melted cheese. Cheese and wine macaroni cheese cow blue castello ricotta cow squirty cheese jarlsberg. Cheesy feet cheesecake cheese and wine cheese on toast cheese slices taleggio cheese on toast cheese triangles. Stinking bishop cheesecake cream cheese say cheese bavarian bergkase.
-
-Cream cheese airedale cheddar. Gouda monterey jack cream cheese port-salut st. agur blue cheese roquefort dolcelatte gouda. Roquefort cow rubber cheese goat cut the cheese cheese triangles swiss cheese strings. Cheese triangles cheesecake taleggio edam paneer dolcelatte say cheese airedale. Paneer cheddar port-salut caerphilly cheesecake caerphilly chalk and cheese babybel. Paneer caerphilly queso cheese and wine cheesy grin chalk and cheese hard cheese roquefort. Bavarian bergkase cottage cheese swiss cheese triangles cheese on toast melted cheese chalk and cheese cheese triangles. Cheese and wine bocconcini manchego monterey jack when the cheese comes out everybody's happy stinking bishop cheese slices who moved my cheese. Boursin cut the cheese cream cheese roquefort stilton cheesy grin pecorino port-salut. Cheese and biscuits.
 
     </div>
     <div style={{ height: "400px"}}></div>
